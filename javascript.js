7fd0258eb3f706692227790d30ca3979f6cd5701
CoSeCant <1collinscam@gmail.com>
2022-07-05 20:11:45 -0400

    IMPROVED THE USER INTERFACE. REMOVED ALERTS AND ADDED ON-SCREEN TEXT ANIMATIONS AND AN UPDATED SCOREBOARD. STILL NEED TO REMOVE/FIX THE ALERT FOR WINNING THE ENTIRE GAME AND ALSO UPDATE THE UI FOR THAT

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ var winsNeeded
 var userWins = 0
 var computerWins = 0 
 var round = 0
+var roundResult = ""
 
 for (let i = 0; i < rounds; i++) {
     function random() {
@@ -24,37 +25,43 @@ for (let i = 0; i < rounds; i++) {
 
 function Rounds(number) {
     winsNeeded = number
-    alert(`You need to beat the computer ${winsNeeded} times to emerge victorious!`)
+    document.getElementById("round-choice").innerHTML = `You have chosen a best of ${winsNeeded}. Good luck!`;
     console.log(gamesNeeded)
 }
 
+
 function Decision(hand) {
     userInput = hand
-    alert(`You have chosen ${userInput}`)
     console.log(userInput)
     if (userInput === "Paper" && computerChoice[round] === "Rock") {
         userWins += 1
-        alert("You won! Paper beats rock")
+        roundResult = "You won that round"
+        document.getElementById("user-score").innerHTML = `${userWins}`;
     } else if (userInput === "Rock" && computerChoice[round] === "Scissors") {
         userWins += 1
-        alert("You won! Rock beats scissors")
+        roundResult = "You won that round"
+        document.getElementById("user-score").innerHTML = `${userWins}`;
     } else if (userInput === "Scissors" && computerChoice[round] === "Paper") {
         userWins += 1
-        alert("You won! Scissors beats paper")
+        roundResult = "You won that round"
+        document.getElementById("user-score").innerHTML = `${userWins}`;
     } else if (computerChoice[round] === "Paper" && userInput === "Rock") {
         computerWins += 1
-        alert("You lost! Paper beats rock")
+        roundResult = "You lost that round"
+        document.getElementById("comp-score").innerHTML = `${computerWins}`;
     } else if (computerChoice[round] === "Rock" && userInput === "Scissors") {
         computerWins += 1
-        alert("You lost! Rock beats scissors")
+        roundResult = "You lost that round"
+        document.getElementById("comp-score").innerHTML = `${computerWins}`;
     } else if (computerChoice[round] === "Scissors" && userInput === "Paper") {
         computerWins += 1
-        alert("You lost! Scissors beats paper")
+        roundResult = "You lost that round"
+        document.getElementById("comp-score").innerHTML = `${computerWins}`;
     } else if (computerChoice[round] === userInput) {
-        alert("You tied! Try again")
+        roundResult = "You tied that round"
     } 
-    alert(`You have won ${userWins} games, while the computer has won ${computerWins}`)
     round += 1
+    document.getElementById("round-result").innerHTML = roundResult;
     if (userWins === winsNeeded) {
         alert(`You succesfully conquered the computer with a final score of: ${userWins} to ${computerWins}`)
     } else if (computerWins === winsNeeded) {
@@ -62,7 +69,6 @@ function Decision(hand) {
     }
 }
 
-
-
+  
 
 
